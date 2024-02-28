646cef2da9a62b4e3a9e2bca1a0b38ad7a902f24
VIVEK MANGAL <138361918+vivekmangal204@users.noreply.github.com>
2024-02-28 13:51:55 +0530

    done

--- amazon-clone/src/App.js
+++ amazon-clone/src/App.js
@@ -1,8 +1,21 @@
 import './App.css';
-
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Home from './components/Home';
 function App() {
   return (
-   <h1>Amazon Clone</h1> 
+  //  <h1>Amazon Clone</h1> 
+  <div className='app'>
+    <Router>
+      <Switch>
+        <Route path='/'>
+          <Home />
+        </Route>
+        {/* <Route path='/cart'>
+
+        </Route> */}
+      </Switch>
+    </Router>
+  </div>
   );
 }
 
